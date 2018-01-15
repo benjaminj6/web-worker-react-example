000057ee83c463a74336a4e57a952ac9631ca409
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-01-14 17:41:32 -0800

    Styling updates 👩🏻🎨

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { css } from 'emotion'
 
 const Worker = require('worker-loader!../counter-worker.js')
 
@@ -6,6 +7,38 @@ class WebWorkerCounter extends Component {
   state = {
     count: 0
   }
+
+  style = css`
+    display: grid;
+    grid-template-columns: 1fr auto;
+    grid-template-rows: [row-start] 1fr 1fr 1fr [row-end];
+    grid-row-gap: 0.5rem;
+
+    h1 {
+      font-size: 4rem;
+      margin: 1.5em 0;
+      grid-row: row-start / row-end;
+      text-align: center;
+    }
+
+    button {
+      --fill: var(--btn-color, #000);
+      --transition: 250ms ease-in-out;
+
+      grid-column: 2;
+      background: transparent;
+      border: 4px solid var(--fill);
+      font-family: sans-serif;
+      border-radius: 2px;
+      font-size: 1.5rem;
+      transition: background var(--transition), color var(--transition);
+
+      &:hover {
+        background: var(--fill);
+        color: #fff;
+      }
+    }
+  `
 
   componentWillMount() {
     this.worker = new Worker()
@@ -21,10 +54,11 @@ class WebWorkerCounter extends Component {
     const { state } = this
 
     return (
-      <div>
+      <div className={this.style}>
         <h1>{state.count}</h1>
 
         <button
+          style={{ '--btn-color': 'green' }}
           onClick={() =>
             this.worker.postMessage({
               type: 'Inc',
@@ -36,6 +70,7 @@ class WebWorkerCounter extends Component {
         </button>
 
         <button
+          style={{ '--btn-color': 'indianred' }}
           onClick={() =>
             this.worker.postMessage({
               type: 'Dec',
@@ -46,7 +81,12 @@ class WebWorkerCounter extends Component {
           -1
         </button>
 
-        <button onClick={() => this.worker.terminate()}>Terminate</button>
+        <button
+          style={{ '--btn-color': 'rebeccapurple' }}
+          onClick={() => this.worker.terminate()}
+        >
+          Terminate
+        </button>
       </div>
     )
   }
